53e5e1373c14a5fa833194a1c168c13f1c16b712
albertonn <albertonnapitupulu12@gmial.com>
2025-02-27 14:28:48 +0700

    done

--- t04_01.c
+++ t04_01.c
@@ -1,25 +1,24 @@
 // 12S24055-THERESIA OKTAVIANI SAMOSIR
 // 12S24056-ENJEL AYUTI NAPITUPULU
 
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
-int main(int _argv, char **_argc) {
-  
-  char msk[61];
-  int i;
-  int ASCII;
-  char ASCII2;
 
-  scanf ("%60s", msk);
-  for (i=0; i< strlen (msk)- 2; i+= 3){
-    sscanf(&msk[i], "%3d", &ASCII);
-    ASCII2 = (char) ASCII;
-    printf("%c", ASCII2);
-  }
+int main() {
+    char str[61];  // Karena panjang maksimal string adalah 60
+    scanf("%s", str);  // Membaca input
+    
+    int len = strlen(str);  // Menghitung panjang string
+    
+    for (int i = 0; i < len; i += 3) {
+        // Ambil 3 karakter dari string
+        char sub[4] = { str[i], str[i+1], str[i+2], '\0' };  // Ambil 3 karakter sebagai string
+        int asciiValue = atoi(sub);  // Mengubah string menjadi angka ASCII
+        printf("%c", (char)asciiValue);  // Cetak karakter yang sesuai dengan ASCII
+    }
 
-  printf ("\n");
-
-  return 0;
+    return 0;
 }
--- t04_02.c
+++ t04_02.c
@@ -2,16 +2,21 @@
 // 12S24056-ENJEL AYUTI NAPITUPULU
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
 
 int main() {
- char str[20];
-  scanf("%20[^\n]", str);
-  
-  for(int i = 0; str[i] != '\0'; i++) {
-    printf("%03d", (int)str[i]);
-  }
-   
-  printf("\n");
-  
-  return 0;
+    char str[21];  // Maksimum 20 karakter + 1 untuk '\0'
+    scanf("%20[^\n]", str); // Membaca input hingga newline
+
+    int len = strlen(str);
+    
+    // Loop setiap karakter dan cetak nilai ASCII dalam format tiga digit
+    for (int i = 0; i < len; i++) {
+        printf("%03d", str[i]);
+    }
+
+    printf("\n");  // Pindah baris setelah output selesai
+    return 0;
 }
